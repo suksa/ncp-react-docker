5f242ec6d91800de3d11dd5ef2f51eb6183c55e0
suksa <40312432+suksa@users.noreply.github.com>
2020-08-25 14:10:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          app <code>src/App.js</code> and save to reload.
+          app <code>src/App.js</code> and save to reload. {process.env.REACT_APP_ENVKEY}
         </p>
         <a
           className="App-link"
